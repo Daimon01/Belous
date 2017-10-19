bc17e0f660f4d6a2df4b7a59507ae3e4bd2b7f83
Daimon01 <Dimas.1990@inbox.ru>
2017-10-19 18:54:17 +0700

    Лаб 1 вся Лаб 2 1-5

---
+++ Лабораторная работа №2/Задание 6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String c, h;
+            c = Console.ReadLine();
+            h = Console.ReadLine();
+            double a, b;
+            a = double.Parse(c);
+            b = double.Parse(h);
+            if (a > 0&&b > 0)
+            {
+                a = a * b / 2;
+                Console.WriteLine("")
+            }
+        }
+    }
+}
