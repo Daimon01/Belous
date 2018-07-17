f102f14950f13da5c32f30488e244e1692a3cc6d
Daimon01 <32871237+Daimon01@users.noreply.github.com>
2018-07-17 19:47:33 +0600

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("new.xml");
+            XmlNodeList list = doc.SelectNodes("/note/heading");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i].InnerText);
+            }
+            list = doc.SelectNodes("/note/body[@test]");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i].InnerText);
+            }
+        }
+    }
+
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StreamReader sr = new StreamReader("new.xml");
+            XmlDocument doc = new XmlDocument();
+            doc.Load("new.xml");
+            for (int i = 0; i < doc.DocumentElement.ChildNodes.Count; i++)
+            {
+                XmlNode root = doc.DocumentElement.ChildNodes[i];
+                if (root.Attributes.Count != 0)
+                {
+                    for (int k = 0; k < root.Attributes.Count; k++)
+                    {
+                        String to = root.Attributes.Item(k).Value;
+                        Console.WriteLine(to);
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("no");
+                }
+            }
+           
+
+            //String to = ;
+        }
+    }
+}
